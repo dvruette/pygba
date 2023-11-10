12e90a6f6156ada9a59d2b34c52c6149c5a76cb0
Dimitri <dimitrivr@icloud.com>
2023-11-10 16:18:27 +0100

    add test for parallel execution + saving

--- test_gba_env.py
+++ test_gba_env.py
@@ -1,6 +1,7 @@
 import json
-import mgba
+import multiprocessing
 
+import mgba
 import mgba.core
 import mgba.vfs
 import mgba.image
@@ -11,9 +12,9 @@ from pygba import PyGBA, PyGBAEnv, PokemonEmerald
 mgba.log.silence()
 
 
-def load_pokemon_game(gba_file: str, autoload_save: bool = True):
-    gba = PyGBA.load(gba_file, autoload_save=autoload_save)
-    if autoload_save:
+def load_pokemon_game(gba_file: str, save_file: str | None = None):
+    gba = PyGBA.load(gba_file, save_file=save_file)
+    if save_file is not None:
         # skip loading screen
         for _ in range(16):
             gba.press_a(30)
@@ -28,21 +29,115 @@ def load_pokemon_game(gba_file: str, autoload_save: bool = True):
 
 
 
+def _run_actions(actions, conn):
+    gba_file = "roms/pokemon_emerald.gba"
+    save_file = "roms/pokemon_emerald.sav"
+    gba = load_pokemon_game(gba_file, save_file=save_file)
+    env = PyGBAEnv(gba, frameskip=15, render_mode="human")
+    for action in actions:
+        obs, reward, done, truncated, info = env.step(env.get_action_id(*action))
+        env.render()
+        conn.send((obs, reward, done, truncated, info))
+
+
+def test_parallel_saving():
+    actions1 = [
+        (None, "start"),
+        (None, None),
+        (None, None),
+        (None, None),
+        ("up", None),
+        (None, None),
+        ("up", None),
+        (None, None),
+        ("up", None),
+        (None, "A"),
+        (None, None),
+        (None, "A"),
+        (None, None),
+        (None, "A"),
+        (None, None),
+        (None, "A"),
+        (None, None),
+        (None, "A"),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, None),
+        (None, "A"),
+        (None, None),
+        (None, "A"),
+        ("right", None),
+        ("right", None),
+        ("left", None),
+        ("left", None),
+        (None, None),
+    ]
+
+    # actions2 = [("left" if i % 8 < 4 else "right", "B") for i in range(len(actions1))]
+    actions2 = actions1.copy()
+
+    # create pipes to communicate with the child processes  
+    parent_conn1, child_conn1 = multiprocessing.Pipe()
+    parent_conn2, child_conn2 = multiprocessing.Pipe()
+
+
+    p1 = multiprocessing.Process(target=_run_actions, args=(actions1, child_conn1))
+    p2 = multiprocessing.Process(target=_run_actions, args=(actions2, child_conn2))
+    p1.start()
+    p2.start()
+
+    received1 = 0
+    received2 = 0
+    while True:
+        if parent_conn1.poll():
+            parent_conn1.recv()
+            received1 += 1
+        if parent_conn2.poll():
+            parent_conn2.recv()
+            received2 += 1
+        if not p1.is_alive() and not p2.is_alive():
+            break
+
+    assert len(actions1) == received1
+    assert len(actions2) == received2
+
+
 def main():
     gba_file = "roms/pokemon_emerald.gba"
-    gba = PyGBA.load(gba_file, autoload_save=False)
+    save_file = "roms/pokemon_emerald.sav"
+    gba = PyGBA.load(gba_file, save_file=save_file)
     env = PyGBAEnv(gba, frameskip=8, render_mode="human")
     env.reset()
     obs, reward, done, truncated, info = env.step(env.get_action_id(None, "A"))
     print(obs.shape, reward, done, info)
 
-    gba = load_pokemon_game(gba_file, autoload_save=True)
+    gba = load_pokemon_game(gba_file, save_file=save_file)
     emerald_wrapper = PokemonEmerald()
     env = PyGBAEnv(gba, emerald_wrapper, frameskip=8, render_mode="human")
     state = emerald_wrapper.game_state(gba)
     del state["pokedex"]
     del state["boxes"]
     print(json.dumps(state, indent=2))
+
+    test_parallel_saving()
 
     actions = [
         ("right", None),
@@ -72,7 +167,7 @@ def main():
     ]
 
     for action in actions:
-        obs, reward, done, info = env.step(env.get_action_id(*action))
+        obs, reward, done, truncated, info = env.step(env.get_action_id(*action))
         print(obs.shape, reward, done, info["game_state"]["pos"])
         env.render()
 
